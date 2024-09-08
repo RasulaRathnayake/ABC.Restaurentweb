152360d97ed3bfa6a212c7e30b3752f4cb1d8086
user <user@rasula>
2024-09-08 14:59:56 +0530

    commit

--- src/main/java/net/javaguides/usersregister/util/DBUtils.java
+++ src/main/java/net/javaguides/usersregister/util/DBUtils.java
@@ -3,21 +3,14 @@ package net.javaguides.usersregister.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import net.javaguides.usersregister.util.DBUtils;
-
 
 public class DBUtils {
-
-    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/your_database";
-    private static final String JDBC_USER = "username";
-    private static final String JDBC_PASSWORD = "password";
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/abc_restaurant"; // Replace with your database URL
+    private static final String JDBC_USER = "root"; // Replace with your DB username
+    private static final String JDBC_PASSWORD = "rasula13675"; // Replace with your DB password
 
     public static Connection getConnection() throws SQLException {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver"); // Ensure the JDBC driver is available
-        } catch (ClassNotFoundException e) {
-            throw new SQLException("JDBC Driver not found", e);
-        }
-        return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
+        Connection connection = DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
+        return connection;
     }
 }
